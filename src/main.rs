197d4b9d21c8a34ef31c3e07bd7810c81ebbb994
Gaute Hope <eg@gaute.vetsj.com>
2022-02-27 15:23:24 +0100

    Blink!

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,48 @@
 #![no_main]
 
 use panic_probe as _;
-use cortex_m_rt::entry;
+use rtt_target::{rtt_init_print, rprintln};
+use cortex_m_rt::{entry, ExceptionFrame, exception};
 
-use stm32l4 as _;
+use stm32l4::stm32l4r5 as pac;
+use stm32l4xx_hal as hal;
+
+use hal::prelude::*;
+use hal::delay::Delay;
 
 #[entry]
 fn main() -> ! {
-    // println!("Hello, world!");
-    loop { }
+    rtt_init_print!();
+    rprintln!("Hello world!");
+
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let mut pwr = dp.PWR.constrain(&mut rcc.apb1r1);
+
+    // let clocks = rcc.cfgr.hclk(8.MHz()).freeze(&mut flash.acr, &mut pwr);
+    let clocks = rcc.cfgr
+        .sysclk(64.MHz())
+        .pclk1(32.MHz())
+        .freeze(&mut flash.acr, &mut pwr);
+
+    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb2);
+
+    // PE2 is connected to the LED
+    let mut led = gpioe.pe2.into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+
+    let mut timer = Delay::new(cp.SYST, clocks);
+
+    loop {
+        rprintln!("loop");
+        led.toggle();
+        timer.delay_ms(1000u32);
+    }
 }
 
+#[exception]
+unsafe fn HardFault(ef: &ExceptionFrame) -> ! {
+    panic!("{:#?}", ef);
+}
